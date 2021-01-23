54ea3211f85d60d4406185d73fe01e7b17b9c3ee
gareththegeek <gj_williams2000@yahoo.co.uk>
2021-01-23 12:21:29 +0000

    Remove unused param

--- doom-map/src/createSectorBufferSetParams.ts
+++ doom-map/src/createSectorBufferSetParams.ts
@@ -2,7 +2,7 @@ import { BufferSetParams } from 'doom-video/dist/buffers/BufferSetParams'
 import { FaceData, SectorData } from './interfaces/SectorData'
 import { Triangulator } from 'pnltri'
 
-const triangulateWall = (face: FaceData, base: number): number[] => [
+const triangulateWall = (base: number): number[] => [
     base + 0,
     base + 1,
     base + 2,
@@ -13,13 +13,14 @@ const triangulateWall = (face: FaceData, base: number): number[] => [
 
 const triangulateFlat = (face: FaceData, base: number): number[] => {
     //TODO floors end up with CCW winding
+    //TODO holes in sectors
     const triangulator = new Triangulator()
     const contour = face.position.map((coord) => ({ x: coord[0], y: coord[2] }))
     return triangulator.triangulate_polygon([contour]).flatMap((face) => face.map((point) => point + base))
 }
 
 const triangulate = (face: FaceData, base: number): number[] =>
-    face.isFlat ? triangulateFlat(face, base) : triangulateWall(face, base)
+    face.isFlat ? triangulateFlat(face, base) : triangulateWall(base)
 
 const buildSectorParams = (sector: SectorData): BufferSetParams => {
     const params: BufferSetParams = { positions: [], indices: [], textures: [] }

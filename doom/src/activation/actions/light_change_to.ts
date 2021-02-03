009c7c832b9a25dd6545d1ca757dc41bc0960b11
gareththegeek <gj_williams2000@yahoo.co.uk>
2021-02-03 10:20:19 +0000

    Update things light level when sector light level changes

--- doom/src/activation/actions/light_change_to.ts
+++ doom/src/activation/actions/light_change_to.ts
@@ -1,5 +1,12 @@
-import { Sector } from 'doom-map'
+import { Sector, Thing } from 'doom-map'
+import { Geometry } from 'doom-video'
+
+const isDefined = (geometry: Geometry | undefined): geometry is Geometry => geometry !== undefined
 
 export const light_change_to = (sector: Sector, amount: number) => {
     sector.lightLevel = amount
+    sector.things
+        .map((thing) => thing.geometry)
+        .filter(isDefined)
+        .forEach((geometry) => (geometry.light = amount))
 }

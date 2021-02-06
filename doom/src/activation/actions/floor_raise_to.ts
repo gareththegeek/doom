fcd10af06b075c2cd81db94629430a4473d377e5
gareththegeek <gj_williams2000@yahoo.co.uk>
2021-02-06 12:29:06 +0000

    Correct next higher floor logic

--- doom/src/activation/actions/floor_raise_to.ts
+++ doom/src/activation/actions/floor_raise_to.ts
@@ -2,6 +2,7 @@ import { Sector } from '../../interfaces/Sector'
 import { raiseFloor } from '../mutations/raiseFloor'
 
 export const floor_raise_to = async (sector: Sector, amount: number) => {
+    console.log(sector, amount)
     await raiseFloor(sector, amount)
     sector.update = undefined
 }
--- doom/src/activation/amounts/next_higher_floor.ts
+++ doom/src/activation/amounts/next_higher_floor.ts
@@ -1,11 +1,10 @@
 import { Sector } from '../../interfaces/Sector'
 import { getAdjacentSectors } from '../../getAdjacentSectors'
-import { highest_floor } from './highest_floor'
 
 export const next_higher_floor = (sector: Sector): number => {
-    const adjacent = getAdjacentSectors(sector)
-    if (adjacent.length < 2) {
-        return highest_floor(sector)
-    }
-    return adjacent.map((sector) => sector.floorHeight).sort((a, b) => b - a)[1]
+    const higher = getAdjacentSectors(sector)
+        .filter((adjacent) => adjacent.floorHeight > sector.floorHeight)
+        .sort((a, b) => a.floorHeight - b.floorHeight)
+
+    return higher[0].floorHeight
 }

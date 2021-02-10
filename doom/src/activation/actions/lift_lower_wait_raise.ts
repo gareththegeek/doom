21fe6f33442a09c1b4514a7c4165d7605b41d296
gareththegeek <gj_williams2000@yahoo.co.uk>
2021-02-10 16:26:26 +0000

    Lifts return to original height

--- doom/src/activation/actions/lift_lower_wait_raise.ts
+++ doom/src/activation/actions/lift_lower_wait_raise.ts
@@ -1,6 +1,5 @@
 import { Sector } from '../../interfaces/Sector'
 import { lowest_floor } from '../amounts'
-import { next_lowest_floor } from '../amounts/next_lowest_floor'
 import { lowerFloor } from '../mutations/lowerFloor'
 import { raiseFloor } from '../mutations/raiseFloor'
 import { wait } from '../mutations/wait'
@@ -8,9 +7,10 @@ import { wait } from '../mutations/wait'
 const LIFT_WAIT = 2
 
 export const lift_lower_wait_raise = async (sector: Sector) => {
+    const start = sector.floorHeight
     if (await lowerFloor(sector, lowest_floor(sector), 140)) return
     if (await wait(sector, LIFT_WAIT)) return
-    if (await raiseFloor(sector, next_lowest_floor(sector), 140)) return
+    if (await raiseFloor(sector, start, 140)) return
 
     sector.update = undefined
 }
